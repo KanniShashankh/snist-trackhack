bf1e730e9e9a29e24f016cd89ded8d6d3583b577
Shashankh Kanni <shashank.kanni@gmail.com>
2023-10-06 11:24:10 +0530

    firebase init

--- frontend/Utils/Urls.ts
+++ frontend/Utils/Urls.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = process.env.NODE_ENV  === 'production' ? 'https://sap-prod.up.railway.app/' :'http://localhost:3000/';
+export const BASE_URL = process.env.NODE_ENV  === 'production' ? 'snist-trackhack-production.up.railway.app/' :'http://localhost:3000/';
 export const BASE_PING = BASE_URL + 'ping';
